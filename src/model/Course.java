be8d5fec29c01ad21b3a2736c67ba760ab6cdda8
Cristian Lasso <crisca_25@hotmail.com>
2019-05-15 11:52:11 -0500

    Test implemented

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
 	
 	private Student root;
@@ -49,12 +52,36 @@ public class Course {
 		}
 		return current;
 	}
-
-	public Student getRoot() {
-		return root;
+	
+	public List<Student> preOrder(){
+		return preOrder(root);
 	}
-
-	public void setRoot(Student root) {
-		this.root = root;
+	
+	private List<Student> preOrder(Student currentNode){
+		List<Student> l = new ArrayList<>();
+		if(currentNode != null) {
+			l.add(currentNode);
+			List<Student> emilioLeft = preOrder(currentNode.getLeft());
+			List<Student> emilioRight = preOrder(currentNode.getRight());
+			l.addAll(emilioLeft);
+			l.addAll(emilioRight);
+		}
+		return l;
+	}
+	
+	public List<Student> indOrder(){
+		return inOrder(root);
+	}
+	
+	public List<Student> inOrder(Student s){
+		List<Student> l = new ArrayList<Student>();
+		if(s!= null) {
+			List<Student> lr = inOrder(s.getRight());
+			List<Student> lf = inOrder(s.getLeft());
+			l.addAll(lr);
+			l.add(root);
+			l.addAll(lf);
+		}
+		return l;
 	}
 }
